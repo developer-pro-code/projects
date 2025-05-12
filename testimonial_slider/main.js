ba9ed90a666d3ffa38529d50fc6f5e70e78d582d
aditya <fordevelopmentusecase@gmail.com>
2025-05-12 12:58:28 +0530

    testimonial slider project dont

--- testimonial_slider/main.js
+++ testimonial_slider/main.js
@@ -6,6 +6,21 @@ async function testimonial() {
     try {
         const respone = await fetch('https://dummyjson.com/quotes');
         const data = await respone.json()
+        // console.log(data)
+        const totalLength = data.quotes.length
+        description.innerHTML = `${data.quotes[0].quote}`
+        let index = 1
+        let intervalId
+        function logging(){
+            if(index >= totalLength){
+                clearInterval(intervalId)
+                intervalId = null
+            }else{
+                description.innerHTML = `${data.quotes[index].quote}`
+                index++
+            }
+        }
+        intervalId = setInterval(logging, 5000);
         
     } catch (error) {
         console.log('something went wrong')
@@ -13,3 +28,4 @@ async function testimonial() {
 }
 
 testimonial()
+    // / opt / cursor / assets / cursor.png;
